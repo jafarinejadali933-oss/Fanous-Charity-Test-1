3c1d756abbffca0253f13d8d91efc7c76ffb0946
jafarinejadali933-oss <jafarinejadali933@gmail.com>
2026-06-30 14:53:12 +0330

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,169 @@
+window.RIVA_PRODUCTS = [
+  {
+    "id": "selma",
+    "name": "ریوا سلما",
+    "latin": "Riva Selma",
+    "price": 5200000,
+    "image": "assets/images/product/Selma/selma1.jpg",
+    "gallery": [
+      "assets/images/product/Selma/selma1.jpg",
+      "assets/images/product/Selma/selma2.jpg",
+      "assets/images/product/Selma/selma3.jpg"
+    ],
+    "desc": "آرام، لطیف و دلبر؛ با پارچه کرپ معروف ریوا و سرآستین های حریر لطیف که به شکل گل طراحی شده اند. انتخابی سبک و خوش فرم برای خانم هایی که خاص بودن را در سادگی می پسندند."
+  },
+  {
+    "id": "aghigh",
+    "name": "ریوا عقیق",
+    "latin": "Riva Aghigh",
+    "price": 5380000,
+    "image": "assets/images/product/Aghigh/aghigh1.jpg",
+    "gallery": [
+      "assets/images/product/Aghigh/aghigh1.jpg",
+      "assets/images/product/Aghigh/aghigh2.jpg",
+      "assets/images/product/Aghigh/aghigh3.jpg",
+      "assets/images/product/Aghigh/aghigh4.jpg"
+    ],
+    "desc": "عبای عقیق ریوا با جزئیاتی که آرام می درخشند؛ ترکیب دانتل دست دوز و نگین کاری ظریف عقیق برای استایلی خاص، مدرن و همیشه ماندگار."
+  },
+  {
+    "id": "javaher-parvaneh",
+    "name": "ریوا جواهر پروانه",
+    "latin": "Riva Javaher-Parvaneh",
+    "price": 5400000,
+    "image": "assets/images/product/Javaher/j-parvaneh1.jpg",
+    "gallery": [
+      "assets/images/product/Javaher/j-parvaneh1.jpg",
+      "assets/images/product/Javaher/j-parvaneh2.jpg"
+    ],
+    "desc": "کرپ اماراتی همراه با پروانه جواهر دوزی شده روی یک دست؛ ظریف، متفاوت و مناسب بانوانی که جزئیات کوچک اما چشمگیر را دوست دارند."
+  },
+  {
+    "id": "parvaneh",
+    "name": "ریوا پروانه",
+    "latin": "Riva Parvaneh",
+    "price": 5300000,
+    "image": "assets/images/product/Parvaneh/parvaneh1.jpg",
+    "gallery": [
+      "assets/images/product/Parvaneh/parvaneh1.jpg",
+      "assets/images/product/Parvaneh/parvaneh2.jpg"
+    ],
+    "desc": "کرپ مشکی اماراتی با پروانه های گلدوزی شده؛ یک عبای مدرن و لطیف با امضای ظرافت ریوا."
+  },
+  {
+    "id": "shokofeh",
+    "name": "ریوا شکوفه",
+    "latin": "Riva Shokofeh",
+    "price": 5500000,
+    "image": "assets/images/product/Shokofeh/shokofeh1.jpg",
+    "gallery": [
+      "assets/images/product/Shokofeh/shokofeh1.jpg",
+      "assets/images/product/Shokofeh/shokofeh2.jpg",
+      "assets/images/product/Shokofeh/shokofeh3.jpg"
+    ],
+    "desc": "کرپ مشکی اماراتی با اپلیکه شکوفه گلدوزی؛ طراحی سبک، زنانه و آرام برای استایل مذهبی مدرن."
+  },
+  {
+    "id": "bahar",
+    "name": "ریوا بهار",
+    "latin": "Riva Bahar",
+    "price": 5200000,
+    "image": "assets/images/product/Bahar/bahar1.jpg",
+    "gallery": [
+      "assets/images/product/Bahar/bahar1.jpg",
+      "assets/images/product/Bahar/bahar2.jpg",
+      "assets/images/product/Bahar/bahar3.jpg"
+    ],
+    "desc": "کرپ مشکی اماراتی به همراه تور گلدوزی شده و مغزی دوزی کرم؛ لطیف، مرتب و مناسب استفاده روزمره و مهمانی های ساده."
+  },
+  {
+    "id": "code-067",
+    "name": "ریوا کد ۰۶۷",
+    "latin": "Riva Code 067",
+    "price": 5000000,
+    "image": "assets/images/product/067/1-067.jpg",
+    "gallery": [
+      "assets/images/product/067/1-067.jpg",
+      "assets/images/product/067/2-067.jpg",
+      "assets/images/product/067/3-067.jpg"
+    ],
+    "desc": "کرپ عبایی با یقه و مچ کتان لمه سفید. این عبا جلو بسته است و زیپ مخفی ۱۵ سانتی از پشت دارد؛ ساده، پوشیده و خوش فرم."
+  },
+  {
+    "id": "code-059",
+    "name": "ریوا کد ۰۵۹",
+    "latin": "Riva Code 059",
+    "price": 5450000,
+    "image": "assets/images/product/059/059-1.jpg",
+    "gallery": [
+      "assets/images/product/059/059-1.jpg",
+      "assets/images/product/059/059-2.jpg",
+      "assets/images/product/059/059-3.jpg"
+    ],
+    "desc": "کرپ عبایی مشکی با مغزی دوزی نقره ای؛ طراحی مینیمال و حرفه ای برای استایلی مرتب و مدرن."
+  },
+  {
+    "id": "code-055",
+    "name": "ریوا کد ۰۵۵",
+    "latin": "Riva Code 055",
+    "price": 5300000,
+    "image": "assets/images/product/055/1-055.jpg",
+    "gallery": [
+      "assets/images/product/055/1-055.jpg"
+    ],
+    "desc": "کرپ عبایی مشکی با مغزی دوزی نقره ای؛ انتخابی ساده، شیک و قابل استفاده برای موقعیت های مختلف."
+  },
+  {
+    "id": "code-054",
+    "name": "ریوا کد ۰۵۴",
+    "latin": "Riva Code 054",
+    "price": 5300000,
+    "image": "assets/images/product/054/1-054.jpg",
+    "gallery": [
+      "assets/images/product/054/1-054.jpg",
+      "assets/images/product/054/2-054.jpg",
+      "assets/images/product/054/0-053.jpg"
+    ],
+    "desc": "کرپ عبایی مشکی با بالاتنه وال نخی گلدوزی شده ذغالی؛ سبک، متفاوت و مناسب استایل پوشیده امروزی."
+  },
+  {
+    "id": "code-051",
+    "name": "ریوا کد ۰۵۱",
+    "latin": "Riva Code 051",
+    "price": null,
+    "image": "assets/images/product/051/1-051.jpg",
+    "gallery": [
+      "assets/images/product/051/1-051.jpg",
+      "assets/images/product/051/2-052.jpg"
+    ],
+    "desc": "این محصول در پوشه تصاویر وجود دارد اما در لیست قیمت اولیه نبود. تا زمان تکمیل قیمت، در حالت معرفی نمایش داده می شود.",
+    "comingSoon": true
+  }
+];
+window.RIVA_SLIDES = [
+  {
+    "product": "selma",
+    "image": "assets/images/slider-image/selma-slider.png",
+    "model": "assets/images/slider-models/model-1.png",
+    "kicker": "Riva Abaya",
+    "title": "سبکی که حال دل را روشن می کند",
+    "text": "ریوا برای پوشش مذهبی مدرن ساخته شده؛ آرام، لطیف و حرفه ای، با جزئیاتی که بی سر و صدا خاص بودن را نشان می دهند."
+  },
+  {
+    "product": "parvaneh",
+    "image": "assets/images/slider-image/parvaneh-slider.png",
+    "model": "assets/images/slider-models/model-2.png",
+    "kicker": "Riva Abaya",
+    "title": "عباهای مدرن برای روزهای آرام",
+    "text": "همه محصولات در همین خانه کنار هم هستند؛ کم تعداد، دقیق طراحی شده و آماده برای استایلی پوشیده و امروزی."
+  },
+  {
+    "product": "aghigh",
+    "image": "assets/images/slider-image/aghigh-slider.png",
+    "model": "assets/images/slider-models/model-3.png",
+    "kicker": "Riva Abaya",
+    "title": "جزئیات ظریف، حس سبک",
+    "text": "از کرپ های خوش فرم تا گلدوزی و جواهر دوزی های آرام؛ هر مدل با نگاه خود ریوا طراحی شده است."
+  }
+];
+window.RIVA_ADMIN = { username: "riva-admin", password: "Riva@2026" };
